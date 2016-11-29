ce670a4d28efa2c35e6ec47cce839dad82617515
Diego <ichigo1991@hotmail.es>
2016-11-29 01:12:40 +0000

    Variables

---
+++ Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim entero As Integer = 1
+        MsgBox(entero)
+        entero = 2
+        MsgBox(entero)
+        Dim numero As Double = 123.456
+        MsgBox(numero)
+        numero = 12.5
+        MsgBox(numero)
+        Dim texto As String = "Hola Mundo"
+        MsgBox(texto)
+        Dim fecha As Date = "26-05-91"
+        MsgBox(fecha)
+
+
+
+
+
+
+    End Sub
+End Class
